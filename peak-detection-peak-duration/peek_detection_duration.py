a733aaf8cc763825118e178ac84afb88c22faebc
Kirankumar Athirala <kirankumarathirala@kirankumars-mbp.speedport.ip>
2022-09-27 23:29:39 +0200

    final commit

--- peak-detection-peak-duration/peek_detection_duration.py
+++ peak-detection-peak-duration/peek_detection_duration.py
@@ -25,13 +25,6 @@ tf.get_logger().setLevel(logging.ERROR)
 
 sys.path.append('src/cnn')
 
-physical_devices = tf.config.list_physical_devices('GPU')
-try:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-except:
-    pass
-import sys
-sys.path.append('src/cnn')
 
 # import modules
 from simulator import Simulator
